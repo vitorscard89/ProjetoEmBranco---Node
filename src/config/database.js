6285c66710276616116192bc1043952866ef1dcd
scard69 <vit.scard@gmail.com>
2019-10-25 00:10:17 -0300

    adição de dependencias e configuração de prettier, eslint e  sequelize(BD)

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: '<dialeto do BD>',
+  host: '<host do BD>',
+  username: '<Username do BD>',
+  password: 'Password do BD',
+  database: '<Database>',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
